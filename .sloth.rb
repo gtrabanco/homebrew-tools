b25bc5cda602b96a5489c92a79e81c67569db040
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-08-08 17:51:27 +0200

    Added all .Sloth dependencies and the initilisation of .Sloth as repository to download submodules dependencies

--- .sloth.rb
+++ .sloth.rb
@@ -3,26 +3,45 @@ class Sloth < Formula
   depends_on "rust"
   depends_on "git-delta"
   depends_on "rust"
+  depends_on "coreutils"
+  depends_on "findutils"
+  depends_on "denisidoro/tools/docpars"
+  depends_on "make" => :recommended
+  depends_on "gnu-sed" => :recommended
+  depends_on "gnu-tar" => :recommended
+  depends_on "gnu-which" => :recommended
+  depends_on "gawk" => :recommended
+  depends_on "grep" => :recommended
   depends_on "bash" => :recommended
+  depends_on "bash-completion@2" => :recommended
   depends_on "zsh" => :recommended
+  depends_on "zsh-completions" => :recommended
   depends_on "git" => :recommended
+  depends_on "python3" => :recommended
   depends_on "python-yq" => :recommended
+  depends_on "mas" => :recommended
+  depends_on "gnutls" => :optional
 
-  version "3.0.6"
+  version "3.0.7"
   desc "Lazy bash for lazy people. Have maintainable dotfiles with .Sloth. A Dotly fork."
   homepage "https://github.com/gtrabanco/.Sloth"
-  url "https://github.com/gtrabanco/.Sloth/archive/refs/tags/#{version}.tar.gz"
-  sha256 "af64e76804fda5b41da6e6d431f2b42fc88b84e616f985781121c0c26b66f2f7"
+  url "https://codeload.github.com/gtrabanco/.Sloth/tar.gz/refs/tags/v#{version}"
+  sha256 "2b91037b36e1d97ff4c52e49e1eecc1b3df8fe9dfb6173b94dafd18c1bc92208"
   license "MIT"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    ohai "Linking dot"
     bin.install "dot"
-    system "bin/dot core install"
+    ohai "Initilisation of .Sloth as repository"
+    system("bin/dot", "core install --only-initilize-sloth")
+    ohai "Create your DOTFILES_PATH with the command:"
+    ohai " $ DOTFILES_PATH=\"${HOME}/.dotfiles\" dot dotfiles create"
+    ohai "Use .Sloth loader by using the command:"
+    ohai " $ dot dotfiles loader --modify"
+    ohai "Restart your terminal to have .Sloth running"
+
+    #ohai "Adding .Sloth to your \`.bashrc\` and \`.zshrc\` files"
+    #system("bin/dot", "core loader --modify")
   end
 
   test do
