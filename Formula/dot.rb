84c7636efd095b27f14849415854fdb01506073f
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-08-22 17:37:14 +0200

    Use of makefile

--- Formula/dot.rb
+++ Formula/dot.rb
@@ -43,6 +43,7 @@ class Dot < Formula
 
   def install
     ENV["SLOTH_PATH"] = "#{prefix}"
+    ENV["INSTALL_PREFIX"] = "#{HOMEBREW_PREFIX}"
     bin.install "bin/dot"
     bin.install "bin/$"
     prefix.install "_raycast"
@@ -59,10 +60,9 @@ class Dot < Formula
       bin.install "bin/open"
     end
 
-    prefix.install Dir["*!bin"]
+    # prefix.install Dir["*!bin"]
 
     cd prefix do
-      #rm_rf "bin/bin"
       rm_rf "scripts/core/version"
       ln_sf "scripts/core", "scripts/self"
     end
@@ -72,7 +72,10 @@ class Dot < Formula
     if build.with? "dotfiles-path"
       ENV["DOTFILES_PATH"] = build.dotfiles_path
       ohai "Installing .Sloth"
-      system "dot", "core", "install"
+      system "make", "install"
+    else
+      ENV["DOTFILES_PATH"] = "#{prefix}/dotfiles_template"
+      system "make", "standalone-install"
     end
   end
 
@@ -82,8 +85,8 @@ class Dot < Formula
         DOTFILES_PATH="${HOME}/.dotfiles" dot dotfiles create
       After that activate .Sloth loader for your zsh & bash shell with:
         dot core loader --modify
-      If you want to use .Sloth only in zsh or bash, see the help to know how to do it:
-        dot core loader --help
+
+        Probably you should uncomment DOTFILES_PATH variable in ~/.bashrc and ~/.zshenv files.
     EOS
   end
 
@@ -103,6 +106,8 @@ index 31a1c02..1abc212 100755
  set -euo pipefail
  
 +export SLOTH_PATH="HOMEBREW_PREFIX/opt/dot"
++export DOTFILES_PATH="${SLOTH_PATH}/dotfiles_template"
++export HOMEBREW_SLOTH=true
 +
  # In Linux we can do this with readlink -f but will fail in macOS and BSD OS
  if [[ -z "${SLOTH_PATH:-${DOTLY_PATH:-}}" || ! -d "${SLOTH_PATH:-${DOTLY_PATH:-}}" ]]; then
