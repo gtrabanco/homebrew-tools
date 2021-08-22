8e5a275e6a6ac0d376f30f3a4ae45d45456f3f26
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-08-23 00:51:14 +0200

    Brew zsh completions is only for macos

--- Formula/dot.rb
+++ Formula/dot.rb
@@ -28,7 +28,6 @@ class Dot < Formula
   depends_on "bash" => :recommended
   depends_on "bash-completion@2" => :recommended
   depends_on "zsh" => :recommended
-  depends_on "zsh-completions" => :recommended
   depends_on "python3" => :recommended
   depends_on "python-yq" => :recommended
   depends_on "gnutls" => :optional
@@ -38,6 +37,7 @@ class Dot < Formula
   end
 
   on_macos do
+    depends_on "zsh-completions" => :recommended
     depends_on "mas" => :recommended
   end
 
